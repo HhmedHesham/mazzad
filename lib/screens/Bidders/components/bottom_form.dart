eebd03e8c52b3609d1b30900a02c6867433ca203
Mahmoud <mm8871918@gmail.com>
2022-07-01 21:41:44 +0200

    place bid

--- lib/screens/Bidders/components/bottom_form.dart
+++ lib/screens/Bidders/components/bottom_form.dart
@@ -1,79 +1,92 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:mazzad/components/default_button.dart';
 import 'package:mazzad/components/dialogs/app_dialog.dart';
 import 'package:mazzad/constants.dart';
 import 'package:mazzad/size_config.dart';
 
 import '../../../controller/auction_controller.dart';
+import '../../../controller/bidders_controller.dart';
 
 class BottomForm extends StatelessWidget {
-  const BottomForm({
+  BottomForm({
     Key? key,
     required this.auction_id,
   }) : super(key: key);
   final int? auction_id;
+  final int? bidPlaced = 0;
+  final TextEditingController? placeBidController = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      mainAxisAlignment: MainAxisAlignment.end,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        Container(
-          alignment: Alignment.centerLeft,
-          padding: const EdgeInsets.only(left: 25),
-          height: getProportionateScreenHeight(53),
-          width: double.infinity,
-          // color: Color.fromARGB(255, 230, 227, 227),
-          color: Colors.white,
-          child: const Text('Can you bid more?',
-              style: TextStyle(color: Colors.black, fontSize: 21)),
-        ),
-        Container(
-          padding: const EdgeInsets.all(10),
-          color: const Color(0xFFC4C4C4),
-          height: getProportionateScreenHeight(80),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Container(
-                width:
-                    getProportionateScreenWidth(SizeConfig.screenWidth / 2.3),
-                decoration: BoxDecoration(
-                    color: Colors.white,
-                    border: Border.all(color: Colors.transparent),
-                    borderRadius: BorderRadius.circular(10)),
-                child: TextFormField(
-                  decoration: InputDecoration(
-                    hintText: 'Enter your bid',
-                    hintStyle: const TextStyle(
-                        color: Color.fromARGB(255, 102, 98, 98)),
-                    contentPadding:
-                        EdgeInsets.all(getProportionateScreenHeight(18)),
-                    isDense: true,
-                  ),
-                  keyboardType:
-                      const TextInputType.numberWithOptions(decimal: true),
-                ),
-              ),
-              Constants.kSmallHorizontalSpacing,
-              Expanded(
-                child: DefaultButton(
-                  onPressed: () {
-                    AppDialog.showAuctionPlacedDialog(
-                        context, 'you have been bid successfully');
-                    AuctionController.recordUserBehavior(
-                        auctionId: auction_id!, action: "bid");
-                  },
-                  text: 'Bid',
-                ),
-              )
-            ],
-          ),
-        ),
-        // SizedBox(height: 5)
-      ],
-    );
+    return GetBuilder<BiddersController>(
+        init: BiddersController(auction_id!),
+        builder: (biddersController) {
+          return (!biddersController.initialized)
+              ? const Center(child: CircularProgressIndicator())
+              : Column(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Container(
+                      alignment: Alignment.centerLeft,
+                      padding: const EdgeInsets.only(left: 25),
+                      height: getProportionateScreenHeight(53),
+                      width: double.infinity,
+                      // color: Color.fromARGB(255, 230, 227, 227),
+                      color: Colors.white,
+                      child: const Text('Can you bid more?',
+                          style: TextStyle(color: Colors.black, fontSize: 21)),
+                    ),
+                    Container(
+                      padding: const EdgeInsets.all(10),
+                      color: const Color(0xFFC4C4C4),
+                      height: getProportionateScreenHeight(80),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: <Widget>[
+                          Container(
+                            width: getProportionateScreenWidth(
+                                SizeConfig.screenWidth / 2.3),
+                            decoration: BoxDecoration(
+                                color: Colors.white,
+                                border: Border.all(color: Colors.transparent),
+                                borderRadius: BorderRadius.circular(10)),
+                            child: TextFormField(
+                              controller: placeBidController,
+                              decoration: InputDecoration(
+                                hintText: 'Enter your bid',
+                                hintStyle: const TextStyle(
+                                    color: Color.fromARGB(255, 102, 98, 98)),
+                                contentPadding: EdgeInsets.all(
+                                    getProportionateScreenHeight(18)),
+                                isDense: true,
+                              ),
+                              keyboardType:
+                                  const TextInputType.numberWithOptions(
+                                      decimal: true),
+                            ),
+                          ),
+                          Constants.kSmallHorizontalSpacing,
+                          Expanded(
+                            child: DefaultButton(
+                              onPressed: () {
+                                biddersController.placeBid(auction_id!, int.parse(placeBidController!.text.toString()));
+                                AppDialog.showAuctionPlacedDialog(
+                                    context, 'you have been bid successfully');
+                                AuctionController.recordUserBehavior(
+                                    auctionId: auction_id!, action: "bid");
+                              },
+                              text: 'Bid',
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                    // SizedBox(height: 5)
+                  ],
+                );
+        });
   }
 }
